a812b8bc918d27495798766974c4eed42f674af9
nguyenduy <nguyenduy011201@gmail.com>
2024-02-21 22:43:30 +0700

    update app inteface auth login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  public value: string | undefined
+  public email: string = '';
+  public password: string = '';
+  constructor(private messageService: MessageService) {}
+
+  public onLogin(): void {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Message Content',
+    });
+  }
 }
