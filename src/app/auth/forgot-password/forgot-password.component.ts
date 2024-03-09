7ab6b9832a9f12c4b31c0f2b180377999125d655
nguyenduy <nguyenduy011201@gmail.com>
2024-03-09 13:21:38 +0700

    update v1 auth

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MessageService} from "primeng/api";
+import {CONSTANT} from "../../core/settings/const.setting";
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,4 +8,47 @@ import { Component } from '@angular/core';
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss',
 })
-export class ForgotPasswordComponent {}
+export class ForgotPasswordComponent {
+  // Default variable
+  public isSendVerifyCode: boolean = false;
+  public isRule: boolean = true;
+  public isStep: boolean = true;
+
+  public auth: any = {
+    email: '',
+    password: '',
+    rePassword: '',
+    verifyCode: '',
+  };
+
+  constructor(private messageService: MessageService) {
+  }
+
+  ngOnInit() {
+  }
+
+  public onChangeRule(): void {
+    this.isRule = !this.isRule;
+  }
+
+  public onChangeStep(): void {
+    this.isStep = !this.isStep;
+  }
+
+  public onReSendVerifyCode(): void {
+    this.isSendVerifyCode = !this.isSendVerifyCode;
+  }
+
+  public onForgotPassword(): void {
+    // console.log('====================================');
+    // console.log(this.authCandidate);
+    // console.log(this.authEmployer);
+    // console.log('====================================');
+
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Message Content',
+    });
+  }
+}
