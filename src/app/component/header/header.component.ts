56269b9043da1941b7f151e72eb8820ce77a2760
nguyenduy <nguyenduy011201@gmail.com>
2024-04-15 10:32:49 +0700

    feat(component): add style, interface for header, footer, page-not-found component

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Data } from '@angular/router';
+import { Router } from '@angular/router';
+import { CONSTANT } from '../../core/settings/const.setting';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
-  showHeader: boolean = false;
+  SYSTEM_PAGE = CONSTANT.SYSTEM_PAGE;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  public onNextPage(key: string): void {
+    this.router.navigate([key]);
+  }
+
+  public isRouteActive(routePath: string): boolean {
+    return this.router.url.includes(routePath);
   }
 }
