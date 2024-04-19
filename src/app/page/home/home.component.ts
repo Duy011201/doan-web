bc983cc1ebc53d2a5c690e5071afc816264c2e1a
nguyenduy <nguyenduy011201@gmail.com>
2024-04-19 23:00:18 +0700

    fix(home): change border-round-xl to border-round

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxChartsModule, Color, LegendPosition } from '@swimlane/ngx-charts';
-import { PrimengModule } from '../core/modules/primeng.module';
-import { CONSTANT } from '../core/settings/const.setting';
+import { PrimengModule } from '../../core/modules/primeng.module';
+import { CONSTANT } from '../../core/settings/const.setting';
 
 @Component({
   selector: 'app-home',
