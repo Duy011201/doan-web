bfb136d65d0ab238edb5c68ace2c451463d711b7
nguyenduy <nguyenduy011201@gmail.com>
2024-04-20 22:25:24 +0700

    improve(auth): improve constant route

--- src/app/auth/auth.routes.module.ts
+++ src/app/auth/auth.routes.module.ts
@@ -4,23 +4,24 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { PageNotFoundComponent } from '../component/page-not-found/page-not-found.component';
+import { CONSTANT } from '../core/settings/const.setting';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: CONSTANT.SYSTEM_PAGE.AUTH_LOGIN,
     component: LoginComponent,
   },
   {
-    path: 'register',
+    path: CONSTANT.SYSTEM_PAGE.AUTH_REGISTER,
     component: RegisterComponent,
   },
   {
-    path: 'forgot-password',
+    path: CONSTANT.SYSTEM_PAGE.AUTH_FORGOT_PASSWORD,
     component: ForgotPasswordComponent,
   },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: CONSTANT.SYSTEM_PAGE.AUTH_LOGIN, pathMatch: 'full' },
   {
-    path: '**',
+    path: CONSTANT.SYSTEM_PAGE.RELATED_404,
     component: PageNotFoundComponent,
   },
 ];
