765330841aa07cc58e498f6fafcbb71191a22e48
nguyenduy <nguyenduy011201@gmail.com>
2024-04-23 22:39:54 +0700

    improve(admin): add dashboard admin

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuLeftComponent } from '../menu-left/menu-left.component';
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
+  selector: 'app-admin-dashboard',
+  standalone: false,
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-
+  ngOnInit() {}
 }
