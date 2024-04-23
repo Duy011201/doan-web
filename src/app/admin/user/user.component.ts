aecfd15ae7db8d67f47e40f3bc9c7a01205699d4
nguyenduy <nguyenduy011201@gmail.com>
2024-04-23 22:40:14 +0700

    improve(admin): add user admin

--- src/app/admin/user/user.component.ts
+++ src/app/admin/user/user.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {LazyLoadEvent} from 'primeng/api';
+import {Table} from 'primeng/table';
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  imports: [],
+  selector: 'app-admin-user',
+  standalone: false,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
+  customers = [{
+    id: 1000,
+    name: 'James Butt',
+    country: {
+      name: 'Algeria',
+      code: 'dz'
+    },
+    company: 'Benton, John B Jr',
+    date: '2015-09-13',
+    status: 'unqualified',
+    verified: true,
+    activity: 17,
+    representative: {
+      name: 'Ioni Bowcher',
+      image: 'ionibowcher.png'
+    },
+    balance: 70663
+  }]
 
+  loading: boolean = false;
+
+  activityValues: number[] = [0, 100];
+
+
+  ngOnInit() {
+  }
+
+  clear(table: Table) {
+    table.clear();
+  }
 }
