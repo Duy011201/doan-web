d26d588945d23c1acd5f5e4cd8556f60115fb297
nguyenduy <nguyenduy011201@gmail.com>
2024-06-05 09:45:40 +0700

    improve(auth): fix register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { MessageService } from 'primeng/api';
-import { CONSTANT } from '../../core/settings/const.setting';
-import DVHCVN from '../../core/settings/json/dvhcvn.json';
+import {Component, OnInit} from '@angular/core';
+import {MessageService} from 'primeng/api';
+import {CONSTANT} from '../../core/settings/const.setting';
 import {
   containsSpecialCharacter,
   containsSpecialOrLetter,
   isEmail,
   isEmpty,
+  isPassword,
+  trimStringObject
 } from '../../core/commons/func';
-import { AuthService } from '../auth.service';
-import { Router } from '@angular/router';
+import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -19,223 +20,158 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
   public SYSTEM_PAGE = CONSTANT.SYSTEM_PAGE;
-
-  // Default variable
-  public isCandidate: boolean = true;
-  public isRule: boolean = true;
-
-  public listCompanyField: any[] | undefined;
-  public listCompanyProvince: any[] | undefined;
-
+  public TYPE_ROLE = {
+    CANDIDATE: 'candidate',
+    EMPLOYER: 'employer'
+  }
   public authCandidate: any = {
     email: '',
     password: '',
-    rePassword: '',
-    verifyCode: '',
-    isVerifyCode: false,
-  };
+    confirmPassword: '',
 
+    verifyCode: '',
+    isPolicy: true,
+    isStep: false,
+  };
   public authEmployer: any = {
     email: '',
     password: '',
-    rePassword: '',
+    confirmPassword: '',
+
     verifyCode: '',
-    isVerifyCode: false,
+    isPolicy: true,
     isStep: false,
+
     companyName: '',
-    companyField: undefined,
-    companyProvince: undefined,
-    companyEmail: '',
     companyCorporateTaxCode: '',
-  };
-
-  public STEP_KEY: any = {
-    NEXT: 'next',
-    PREV: 'prev',
-  };
-
-  public VERIFY_KEY: any = {
-    CANDIDATE: 'candidate',
-    EMPLOYER: 'employer',
   };
 
   constructor(
     private messageService: MessageService,
     private authService: AuthService,
     private router: Router
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
-    this.listCompanyField = CONSTANT.COMPANY_FIELD;
-    this.listCompanyProvince = DVHCVN.data;
   }
 
   public onNextPage(key: string): void {
     this.router.navigate([key]);
   }
 
-  public onChangeCandidate(): void {
-    this.isCandidate = !this.isCandidate;
-  }
+  private validAuthInput(type: string): boolean {
+    let errorMessage = '';
 
-  public onChangeRule(): void {
-    this.isRule = !this.isRule;
-  }
-
-  public onChangeVerifyCode(key: string, step: string, resend: boolean): void {
-    const errorMessage = this.isValidAuth();
-    let email;
-
-    if (!isEmpty(errorMessage)) {
-      this.onRegister();
-      return;
-    }
-
-    if (key === this.VERIFY_KEY.CANDIDATE) {
-      if (!resend)
-        this.authCandidate.isVerifyCode = !this.authCandidate.isVerifyCode;
-      email = this.authCandidate.email;
-    } else if (key === this.VERIFY_KEY.EMPLOYER) {
-      if (!resend)
-        this.authEmployer.isVerifyCode = !this.authEmployer.isVerifyCode;
-      email = this.authEmployer.email;
-    }
-
-    if (step === this.STEP_KEY.NEXT) {
-      this.authService.verifyCode({ email: email }).subscribe(
-        (result: any) => {
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Success',
-            detail: result['message'],
-          });
-        },
-        (error: any) => {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: error.error.message,
-          });
-        }
-      );
-    }
-  }
-
-  public onChangeStep(key: object): void {
-    if (key !== this.STEP_KEY.PREV) {
-      const errorMessage = this.isValidAuth();
-      if (!isEmpty(errorMessage)) {
-        this.onRegister();
-        return;
-      }
-    }
-
-    // Reset default
-    this.authEmployer.companyField = undefined;
-    this.authEmployer.companyProvince = undefined;
-
-    this.authEmployer.isStep = !this.authEmployer.isStep;
-  }
-
-  public onChangeAuthEmployer(key: string, variable: string, value: any): void {
-    this.authEmployer[key] = value?.[variable];
-  }
-
-  private isValidAuth(): string {
-    if (this.isCandidate) {
+    if (type === this.TYPE_ROLE.CANDIDATE) {
       if (!isEmail(this.authCandidate.email)) {
-        return CONSTANT.SYSTEM_MESSAGE.INVALID_EMAIL_FORMAT;
-      } else if (
-        isEmpty(this.authCandidate.password) ||
-        this.authCandidate.password.length < 5 ||
-        this.authCandidate.password.length > 20
-      ) {
-        return CONSTANT.SYSTEM_MESSAGE.INVALID_PASSWORD_FORMAT;
-      } else if (
-        this.authCandidate.password !== this.authCandidate.rePassword
-      ) {
-        return CONSTANT.SYSTEM_MESSAGE.INVALID_PASSWORD_NOT_MATCH;
+        errorMessage = CONSTANT.SYSTEM_MESSAGE.INVALID_EMAIL_FORMAT;
+      } else if (isPassword(this.authCandidate.password)) {
+        errorMessage = CONSTANT.SYSTEM_MESSAGE.INVALID_PASSWORD_FORMAT;
+      } else if (this.authCandidate.password !== this.authCandidate.confirmPassword) {
+        errorMessage = CONSTANT.SYSTEM_MESSAGE.INVALID_PASSWORD_NOT_MATCH;
+      } else if (isEmpty(this.authCandidate.isPolicy)) {
+        errorMessage = CONSTANT.SYSTEM_MESSAGE.INVALID_POLICY;
       }
-    }
-
-    if (!this.isCandidate) {
+    } else if (type === this.TYPE_ROLE.EMPLOYER) {
       if (!isEmail(this.authEmployer.email)) {
-        return CONSTANT.SYSTEM_MESSAGE.INVALID_EMAIL_FORMAT;
-      } else if (
-        isEmpty(this.authEmployer.password) ||
-        this.authEmployer.password.length < 5 ||
-        this.authEmployer.password.length > 20
-      ) {
-        return CONSTANT.SYSTEM_MESSAGE.INVALID_PASSWORD_FORMAT;
-      } else if (this.authEmployer.password !== this.authEmployer.rePassword) {
-        return CONSTANT.SYSTEM_MESSAGE.INVALID_PASSWORD_NOT_MATCH;
-      }
-
-      if (this.authEmployer.isStep) {
-        if (
-          isEmpty(this.authEmployer.companyName) ||
-          containsSpecialCharacter(this.authEmployer.companyName)
-        ) {
-          return CONSTANT.SYSTEM_MESSAGE.INVALID_COMPANY_NAME_FORMAT;
-        } else if (!isEmail(this.authEmployer.companyEmail)) {
-          return CONSTANT.SYSTEM_MESSAGE.INVALID_EMAIL_FORMAT;
-        } else if (
-          isEmpty(this.authEmployer.companyCorporateTaxCode) ||
-          containsSpecialOrLetter(this.authEmployer.companyCorporateTaxCode)
-        ) {
-          return CONSTANT.SYSTEM_MESSAGE.INVALID_COMPANY_CORPORATE_TAX_CODE;
-        } else if (isEmpty(this.authEmployer.companyField)) {
-          return CONSTANT.SYSTEM_MESSAGE.INVALID_COMPANY_FIELD;
-        } else if (isEmpty(this.authEmployer.companyProvince)) {
-          return CONSTANT.SYSTEM_MESSAGE.INVALID_COMPANY_PROVINCE;
-        }
+        errorMessage = CONSTANT.SYSTEM_MESSAGE.INVALID_EMAIL_FORMAT;
+      } else if (isPassword(this.authEmployer.password)) {
+        errorMessage = CONSTANT.SYSTEM_MESSAGE.INVALID_PASSWORD_FORMAT;
+      } else if (this.authEmployer.password !== this.authEmployer.confirmPassword) {
+        errorMessage = CONSTANT.SYSTEM_MESSAGE.INVALID_PASSWORD_NOT_MATCH;
+      } else if (isEmpty(this.authEmployer.companyName)
+        || containsSpecialCharacter(this.authEmployer.companyName)) {
+        errorMessage = CONSTANT.SYSTEM_MESSAGE.INVALID_COMPANY_NAME_FORMAT;
+      } else if (isEmpty(this.authEmployer.companyCorporateTaxCode)
+        || containsSpecialOrLetter(this.authEmployer.companyCorporateTaxCode)) {
+        errorMessage = CONSTANT.SYSTEM_MESSAGE.INVALID_COMPANY_CORPORATE_TAX_CODE;
+      } else if (isEmpty(this.authEmployer.isPolicy)) {
+        errorMessage = CONSTANT.SYSTEM_MESSAGE.INVALID_POLICY;
       }
     }
 
-    if (isEmpty(this.isRule)) {
-      return CONSTANT.SYSTEM_MESSAGE.INVALID_RULE;
-    }
-
-    return '';
-  }
-
-  public onRegister(): void {
-    const errorMessage = this.isValidAuth();
     if (!isEmpty(errorMessage)) {
       this.messageService.add({
         severity: 'error',
         summary: 'Error',
         detail: errorMessage,
       });
-      return;
-    } else {
-      let payload;
+      return false;
+    }
 
-      if (this.isCandidate) {
-        payload = {
-          email: this.authCandidate.email,
-          password: this.authCandidate.password,
-          verifyCode: this.authCandidate.verifyCode,
-          role: 'candidate',
-          desc: 'Ứng viên',
-        };
-      } else {
-        payload = {
-          email: this.authEmployer.email,
-          password: this.authEmployer.password,
-          role: 'employer',
-          desc: 'Nhà tuyển dụng',
-          verifyCode: this.authCandidate.verifyCode,
-          companyName: this.authEmployer.companyName,
-          companyField: this.authEmployer.companyField,
-          companyProvince: this.authEmployer.companyProvince,
-          companyEmail: this.authEmployer.companyEmail,
-          companyCorporateTaxCode: this.authEmployer.companyCorporateTaxCode,
-        };
+    return true;
+  }
+
+  public onRegisterCandidate(): void {
+    if (this.validAuthInput(this.TYPE_ROLE.CANDIDATE)) {
+      this.authCandidate = trimStringObject(this.authCandidate);
+      const payload = {
+        email: this.authCandidate.email,
+        password: this.authCandidate.password,
+        verifyCode: this.authCandidate.verifyCode,
+        role: this.TYPE_ROLE.CANDIDATE,
+      };
+      this.apiRegister(payload);
+    }
+  }
+
+  public onRegisterEmployer(): void {
+    if (this.validAuthInput(this.TYPE_ROLE.EMPLOYER)) {
+      this.authEmployer = trimStringObject(this.authEmployer);
+      const payload = {
+        email: this.authEmployer.email,
+        password: this.authEmployer.password,
+        role: this.TYPE_ROLE.EMPLOYER,
+        verifyCode: this.authCandidate.verifyCode,
+        companyName: this.authEmployer.companyName,
+        companyEmail: this.authEmployer.companyEmail,
+        companyCorporateTaxCode: this.authEmployer.companyCorporateTaxCode,
+      };
+      this.apiRegister(payload);
+    }
+  }
+
+  onVerifyCode(type: string): void {
+    if (type === this.TYPE_ROLE.CANDIDATE && this.validAuthInput(type))
+      this.apiVerifyCode(type, {email: this.authCandidate.email});
+
+    if (type === this.TYPE_ROLE.EMPLOYER && this.validAuthInput(type))
+      this.apiVerifyCode(type, {email: this.authEmployer.email});
+  }
+
+  apiVerifyCode(type: string, payload: any): void {
+    this.authService.verifyCode(payload).subscribe(
+      (result: any) => {
+        if (result.status === CONSTANT.SYSTEM_STATUS_CODE.OK) {
+
+          if (type === this.TYPE_ROLE.CANDIDATE)
+            this.authCandidate.isStep = true;
+
+          if (type === this.TYPE_ROLE.EMPLOYER)
+            this.authEmployer.isStep = true;
+
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: result['message'],
+          });
+        }
+      },
+      (error: any) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: error.error.massage || error.error.message,
+        });
       }
+    );
+  }
 
-      this.authService.register(payload).subscribe(
-        (result: any) => {
+  apiRegister(payload: any): void {
+    this.authService.register(payload).subscribe(
+      (result: any) => {
+        if (result.status === CONSTANT.SYSTEM_STATUS_CODE.OK) {
           this.messageService.add({
             severity: 'success',
             summary: 'Success',
@@ -244,15 +180,15 @@ export class RegisterComponent implements OnInit {
           setTimeout(() => {
             this.router.navigate(['/auth/login']);
           }, 2000);
-        },
-        (error: any) => {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: error.error.message,
-          });
         }
-      );
-    }
+      },
+      (error: any) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: error.error.massage || error.error.message,
+        });
+      }
+    );
   }
 }
