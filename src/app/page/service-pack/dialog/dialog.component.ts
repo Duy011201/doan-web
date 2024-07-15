df98d834c1956bc7228389fe2add20519eef94ba
nguyenduy <nguyenduy011201@gmail.com>
2024-07-15 14:29:22 +0700

    improve(component): fix code name little

--- src/app/page/service-pack/dialog/dialog.component.ts
+++ src/app/page/service-pack/dialog/dialog.component.ts
@@ -13,7 +13,7 @@ import {
 import { environment } from '../../../core/environments/develop.environment';
 
 @Component({
-  selector: 'app-admin-service-pack-dialog',
+  selector: 'app-service-pack-dialog',
   standalone: false,
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss',
