56482a34226cad76a45d0981e60fd721f3526e68
nguyenduy <nguyenduy011201@gmail.com>
2024-07-15 15:56:01 +0700

    feat(cart): add cart component

---
+++ src/app/page/cart/dialog/dialog.component.ts
@@ -0,0 +1,79 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { SETTING } from '../../../core/configs/setting.config';
+import { CONSTANT } from '../../../core/configs/constant.config';
+import { PageService } from '../../page.service';
+import {
+  getFromLocalStorage,
+  isEmail,
+  isEmpty,
+  removeQuotes,
+  trimStringObject,
+} from '../../../core/commons/func';
+import { environment } from '../../../core/environments/develop.environment';
+
+@Component({
+  selector: 'app-cart-dialog',
+  standalone: false,
+  templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.scss',
+})
+export class DialogCartComponent implements OnInit {
+  @Input() visible: boolean = false;
+  @Input() data: any = {};
+  @Output() visibleChange = new EventEmitter<boolean>();
+
+  SYSTEM_ACTION = SETTING.SYSTEM_ACTION;
+  PRODUCT_STATUS = CONSTANT.PRODUCT_STATUS;
+
+  selectStatus: any = {};
+
+  constructor(
+    private messageService: MessageService,
+    private service: PageService
+  ) {}
+
+  ngOnInit() {}
+
+  onHideDialog() {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
+  }
+
+  ngOnChanges() {}
+
+  public async onUpdateProduct(): Promise<void> {
+    const updatedBy = removeQuotes(getFromLocalStorage('userID'));
+
+    this.data = trimStringObject(this.data);
+    const payload = {
+      productID: this.data.productID,
+      status: this.PRODUCT_STATUS[1].CODE || this.data.status,
+      updatedBy: updatedBy,
+    };
+
+    this.apiUpdate(payload);
+  }
+
+  apiUpdate(payload: any) {
+    this.service.updateProduct(payload).subscribe(
+      (result: any) => {
+        if (result.status === SETTING.SYSTEM_HTTP_STATUS.OK) {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: result.message,
+          });
+          this.onHideDialog();
+        }
+      },
+      (error: any) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: error.error.massage || error.error.message,
+        });
+      }
+    );
+  }
+}
