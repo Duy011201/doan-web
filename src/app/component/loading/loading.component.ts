df7f203f2fb4e4a52b171bafd974324490f66c4a
nguyenduy <nguyenduy011201@gmail.com>
2024-07-21 14:27:21 +0700

    feat(loading): add loading component

--- src/app/component/loading/loading.component.ts
+++ src/app/component/loading/loading.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoadingService } from '../../core/services/loading.service';
 
 @Component({
   selector: 'app-loading',
-  standalone: true,
-  imports: [],
   templateUrl: './loading.component.html',
-  styleUrl: './loading.component.scss'
+  styleUrls: ['./loading.component.css'],
 })
 export class LoadingComponent {
+  isLoading: Observable<boolean>;
 
+  constructor(private loadingService: LoadingService) {
+    this.isLoading = this.loadingService.loading$;
+  }
 }
